2da8c9773a2dc3c5c28dd01533651bf145363e87
Miranda Bisson <47126696+M-Bisson@users.noreply.github.com>
2019-10-24 12:57:37 -0400

    added kittenSearch

--- bot.js
+++ bot.js
@@ -4,12 +4,42 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#cats", count: 10, result_type: "recent"}; 
+// This is the URL of a search for the latest tweets on the '#cats' hashtag.
+var catsSearch = {q: "#cats", count: 10, result_type: "recent"}; 
 
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
+//This is the URl of a search for the latest tweets on the "#kitten' hashtag.
+var kittenSearch = {q: "#kitten", count: 10, result_type: "recent"};
+
+// This function finds the latest tweet with the #cats hashtag, and retweets it.
+function retweetCat() {
+	T.get('search/tweets', catsSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+// This function finds the latest tweet with the #kitten hashtag, and retweets it.
+function retweetKitten() {
+	T.get('search/tweets', kittenSearch, function (error, data) {
 	  // log out any errors and responses
 	  console.log(error, data);
 	  // If our search request to the server had no errors...
@@ -35,7 +65,8 @@ function retweetLatest() {
 }
 
 // Try to retweet something as soon as we run the program...
-retweetLatest();
+retweetCat();
+retweetKitten();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
