e2a245ffe9b297dbab37e069b25b976cfe1dfba9
Miranda Bisson <47126696+M-Bisson@users.noreply.github.com>
2019-10-24 12:59:20 -0400

    updated set interval calls

--- bot.js
+++ bot.js
@@ -69,4 +69,5 @@ retweetCat();
 retweetKitten();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(retweetCat, 1000 * 60 * 60);
+setInterval(retweetKitten, 1000 * 60 * 60);
