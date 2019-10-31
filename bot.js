3a19b23f5469e9a8101a963f852cf6ad9fae0a34
Miranda Bisson <47126696+M-Bisson@users.noreply.github.com>
2019-10-31 12:51:22 -0400

    changed retweet calls

--- bot.js
+++ bot.js
@@ -13,6 +13,9 @@ var kittenSearch = {q: "#kitten", count: 10, result_type: "recent"};
 // This is the URL of a search for the latest tweets on the '#dog' hashtag.
 var dogSearch = {q: "#dog", count: 10, result_type: "recent"}; 
 
+// This is the URL of a search for the latest tweets on the '#puppy' hashtag.
+var puppySearch = {q: "#puppy", count: 10, result_type: "recent"}; 
+
 // This function finds the latest tweet with the #cats hashtag, and retweets it.
 function retweetCat() {
 	T.get('search/tweets', catsSearch, function (error, data) {
@@ -70,6 +73,32 @@ function retweetKitten() {
 // This function finds the latest tweet with the #dog hashtag, and retweets it.
 function retweetDog() {
 	T.get('search/tweets', dogSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+function retweetPuppy() {
+	T.get('search/tweets', puppySearch, function (error, data) {
 	  // log out any errors and responses
 	  console.log(error, data);
 	  // If our search request to the server had no errors...
@@ -227,10 +256,10 @@ function runBot() {
             "Seriously, " + capitalize(singularize(nouns.pick().word)) + " is the only acceptable name for a pet rock.",
             "I can't believe I forgot to bring my " + nouns.pick().word + " with me. It's my dog's favorite toy.",
             "Sooo much homework in this " + capitalize(nouns.pick().word) + " class and my puppy ate it all.",
-            "Yet another distruction! My cat got into my " + nouns.pick().word + " today.",
+            "Yet another distruction! My cat got into my " + nouns.pick().word + " today. Ripped it to shreds.",
             "Seriously I have had enough of Intro to " + capitalize(nouns.pick().word) + ". Can't we just look up pictures of cute baby animals?",
             "Who's coming to Club " + capitalize(singularize(nouns.pick().word)) + " tonight? I heard they will have puppies and " + pluralize(nouns.pick().word) + ".",
-            "Missed class again. Had to find a " + nouns.pick().word + " to appease my unreasonable parrot."
+            "Missed class again. Had to find a " + nouns.pick().word + " to appease my unreasonable parrot.", "If I throw a " + nouns.pick().word + " do you think my dog will chase after it?", "Do you think that cats dream about " + nouns.pick().word + " while they sleep all day?", "Uhhh, I don't think cats are supposed to eat " + nouns.pick().word + ".", "The world is ending! My ferret now has a " + nouns.pick().word + " and that means the end for us all.", "How many " + pluralize(nouns.pick().word) + " is too many for one pet fish?", "Just met my friend's new puppy, " + capitalize(nouns.pick().word) + ". So cute!", "My friend found a cat inside of a " + nouns.pick().word + " today. Don't think that was supposed to be there.", "Just bought a " + nouns.pick().word + " for my bird. I hope he likes it.", "Should I be concerned if my cat keeps bringing me " + nouns.pick().word + "?", pluralize(nouns.pick().word) + " are really the best toys for puppies.", "My dogs are always fighting over who gets to play with the " + nouns.pick().word, "I just discovered a new species of bat. They only eat " + nouns.pick().word
 			// etc.			
 		];
 		
@@ -246,8 +275,8 @@ function runBot() {
 			retweetDog();
 			
 		} else if (rand <= 0.60 && rand > 0.40) {
-			console.log("-------Follow someone who @-mentioned us");
-			followAMentioner();
+			console.log("-------Retweet something tagged with #puppy");
+			retweetPuppy();
 		} else if (rand <= 0.40 && rand > 0.20) {
             console.log("-------Retweet something tagged with #cats");
             retweetCat();
